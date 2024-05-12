9a163db5992902ededd01bcf5b597e1102c56711
Alireza Sarabchi <a.sarabchi@gmail.com>
2024-05-12 17:29:55 +0330

    Refactor code to use const instead of var in content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,11 +1,11 @@
 window.addEventListener("load", function () {
-  var DescListItems = document.querySelectorAll(
+  const DescListItems = document.querySelectorAll(
     ".EditionDetails .DescListItem dt"
   );
 
-  for (var i = 0; i < DescListItems.length; i++) {
+  for (const i = 0; i < DescListItems.length; i++) {
     if (DescListItems[i].textContent === "ISBN") {
-      var ISBN = DescListItems[i].nextElementSibling.textContent;
+      const ISBN = DescListItems[i].nextElementSibling.textContent;
     }
   }
 });
