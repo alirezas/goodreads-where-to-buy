c12e2b7e21adf9b942048fba63973f09f3d279a9
Alireza Sarabchi <a.sarabchi@gmail.com>
2024-05-12 17:22:43 +0330

    feat: Add event listener to retrieve ISBN value on page load

--- scripts/content.js
+++ scripts/content.js
@@ -1,21 +1,11 @@
-const someAction = () => {
-  console.log("content.js loaded");
-};
+window.addEventListener("load", function () {
+  var DescListItems = document.querySelectorAll(
+    ".EditionDetails .DescListItem dt"
+  );
 
-// Select the element containing the ISBN value
-var DescListItems = document.querySelectorAll(
-  ".EditionDetails .DescListItem dt"
-);
-
-for (var i = 0; i < DescListItems.length; i++) {
-  if (DescListItems[i].textContent === "ISBN") {
-    // Select the ISBN value
-    var ISBN = DescListItems[i].nextElementSibling.textContent;
-    // Send the ISBN value to the background script
-    //   chrome.runtime.sendMessage({ ISBN: ISBN });
-    alert("2: " + ISBN);
-    console.log(ISBN);
+  for (var i = 0; i < DescListItems.length; i++) {
+    if (DescListItems[i].textContent === "ISBN") {
+      var ISBN = DescListItems[i].nextElementSibling.textContent;
+    }
   }
-}
-
-document.addEventListener("load", someAction);
+});
